c722bd21cb2dae5bce3d59c084bbc1be5b0264ce
kgautams-123 <gautamsingh07@gmail.com>
2025-02-12 06:09:33 +0530

    Update exposed_notificication_test.py

--- exposed_notificication_test.py
+++ exposed_notificication_test.py
@@ -2,8 +2,8 @@ import boto3
 
 # Create session with credentials
 session = boto3.Session(
-    aws_access_key_id='AKIAY3O4SOO7RP6RSXDO',
-    aws_secret_access_key='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'
+    aws_access_key_id='<abc>',
+    aws_secret_access_key='<def>
 )
 
 # Create S3 resource
